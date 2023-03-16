0870d23abed007b7eba159f3df035d6c03b82de9
muuhaab0 <medhatmuhab@gmail.com>
2023-03-16 21:17:05 +0200

    l

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
   return (
     <div className="App">
        <Routes>
-     <Route path="/" exact element={<Home />} />
+     <Route path="/egy" exact element={<Home />} />
+     <Route path="/"  element={<Home />} />
      <Route path="/service/:id" element={<Services />} />
      <Route path="/dataSecurity" element={<DataSecurity />} />
      <Route path="/solutions" element={<Solutions />} />
