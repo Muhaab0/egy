adc35d41fc74de66422ba93a5c4588b8997aaa0c
muuhaab0 <medhatmuhab@gmail.com>
2023-03-17 00:25:20 +0200

    l

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import App from './App';
 import "./index.css";
 import ReactDOM from 'react-dom/client';
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename='/egy'>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
